f5f18aa1cd9f28ad2b7aa9c6439582bbf20e2c33
loudovsky <luileptitecossais@gmail.com>
2024-01-26 16:25:47 +0100

    last update before the  WE. Seems to work fine now git status cd .. :q

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,30 @@
 let userButton = document.querySelector('.button');
 let userTable = document.querySelector('.table');
 let userInput = document.querySelector('.input');
-let emptyMessage = document.querySelector('.empty-message')
 let nbrItems = 0;
 
 
 function insertItem() {
-    nbrItems++
+    if (userInput.value != "") {
+     nbrItems++
         if (nbrItems != 0) {
-            emptyMessage.innerHTML = "";
+            document.querySelector('.empty-message').innerText = "";
             userTable.innerHTML += `<tr class="row"><td>${userInput.value}</td><td class="right-cell"><span class="cross">❌</span></td></tr>`;
             let crosses = document.querySelectorAll('.cross');
             crosses.forEach(function(cross) {
                 cross.addEventListener('click', function() {
-                    nbrItems--
+                    nbrItems--;
                     cross.closest(".row").remove()
                     crosses = document.querySelectorAll('.cross')
                     if (crosses.length == 0){
-                        userTable.innerHTML = `<td>Votre liste des courses est vide 😋</td><td class="right-cell"></td>`
+                        userTable.innerHTML = `<td class="empty-message">Votre liste des courses est vide 😋</td>`;
+                        nbrItems = 0;
                     }
                 });
             })
         }
-        userInput.value = "" 
+        userInput.value = ""
+    } 
 }
 
 userButton.addEventListener('click', function() {
